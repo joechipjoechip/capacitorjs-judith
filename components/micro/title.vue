a863cc90d26e59e97196741ef442081bc4f8ff44
lio <joechipjoechip@gmail.com>
2022-01-06 12:02:02 +0100

    title : ok

--- components/micro/title.vue
+++ components/micro/title.vue
@@ -1,7 +1,8 @@
 <template>
 	<div class="micro-title">
-
-		<h4>{{ text }}</h4>
+		<span class="faded">{{ text }}</span>
+		<h4 class="consistent">{{ text }}</h4>
+		<span class="faded">{{ text }}</span>
 
 	</div>
 </template>
@@ -20,14 +21,39 @@
 <style lang="scss" scoped>
 
 	.micro-title {
+		position: relative;
 		border: solid 1px green;
 		width: 100%;
 		text-align: center;
 
-		h4 {
-			color: white;
-			font-size: 50px;
+		* {
+			font-family: "PresicavRg";
+			font-size: 18px;
+			color: $colorWhite;
+			display: inline-block;
+			text-transform: uppercase;
+			line-height: 22px;
+			letter-spacing: 7.2px;
 		}
+
+		.faded {
+			opacity: .3;
+		}
+
+		.consistent {
+
+			&::after {
+				content: "";
+				display: block;
+				height: 1px;
+				background-color: $colorWhite;
+				width: 80%;
+				margin: 0 auto;
+				margin-top: 13px;
+			}
+
+		}
+
 
 	}
 
