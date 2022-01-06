8d727a38507cb12d341a2a4cd7ac333b02b7dddb
lio <joechipjoechip@gmail.com>
2022-01-06 14:41:59 +0100

    philosophy : squares ok

--- components/philosophy.vue
+++ components/philosophy.vue
@@ -5,21 +5,25 @@
 
 		<div class="philosophy-wrapper">
 
-			<div class="little-square-outside-1"></div>
+			<div class="philosophy-inner">
 
-			<div class="little-square-outside-2"></div>
+				<div class="big-square">
 
-			<div class="big-square">
+					<div class="little-square-outside-1"></div>
 
-				<div class="little-square-inside-1"></div>
+					<div class="little-square-outside-2"></div>
 
-				<div class="little-square-inside-2"></div>
+					<div class="little-square-inside-1"></div>
+
+					<div class="little-square-inside-2"></div>
+
+					<div class="little-square-outside-3"></div>
+
+					<div class="little-square-outside-4"></div>
+
+				</div>
 
 			</div>
-
-			<div class="little-square-outside-3"></div>
-
-			<div class="little-square-outside-4"></div>
 
 		</div>
 
@@ -54,25 +58,115 @@
 
 <style lang="scss" scoped>
 
-	.philosophy-container {
-		display: block;
-		position: relative;
+	.philosophy {
 
-		margin-top: 150px;
-		margin-bottom: 550px;
+		$innerSize: 700px;
+		$bigSquareSize: 500px;
+		$littleSquareSize: 10%;
 
-		border: solid 1px red;
 
-		*[class*="square"] {
-			border: solid 1px green;
-		}
+		$calcDecayInside: calc($littleSquareSize / 2);
+		$calcDecay1: calc(-50% - ($littleSquareSize / 2));
+		$calcDecay2: calc(50% - ($littleSquareSize / 2));
 
-		.big-square {
+		&-container {
 			display: block;
-			width: 500px;
-			height: 500px;
-			margin: 0 auto;
+			position: relative;
+
+			margin-top: 150px;
+			margin-bottom: 550px;
+
 		}
+
+		&-wrapper {
+			display: block;
+			// here : if we need a global margin-top or bottom
+		}
+
+		&-inner {
+			position: relative;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+
+			width: $innerSize;
+			height: $innerSize;
+			margin: 25% auto;
+
+			transform-origin: center;
+			transform: rotate(45deg);
+
+			// border: solid 1px white;
+
+			.big-square {
+				position: relative;
+				border: solid 1px green;
+
+				display: block;
+				width: $bigSquareSize;
+				height: $bigSquareSize;
+				margin: 0 auto;
+			}
+
+			*[class^="little-square"] {
+				width: $littleSquareSize;
+				height: $littleSquareSize;
+				border: solid 1px transparent;
+			}
+
+			*[class^="little-square-inside"] {
+				border-color: yellow;
+				position: absolute;
+
+				&[class*="-1"] {
+					top: $calcDecayInside;
+					left: $calcDecayInside;
+				}
+
+				&[class*="-2"] {
+					bottom: $calcDecayInside;
+					right: $calcDecayInside;
+				}
+
+			}
+
+			*[class^="little-square-outside"] {
+				border-color: rebeccapurple;
+				position: absolute;
+
+				transform-origin: center;
+				transform: rotate(-45deg);
+
+				&[class*="-1"] {
+					top: $calcDecay1;
+					left: $calcDecay2;
+					border-color: red;
+				}
+
+				&[class*="-2"] {
+					top: $calcDecay2;
+					left: $calcDecay1;
+					border-color: green;
+				}
+
+				&[class*="-3"] {
+					bottom: $calcDecay1;
+					right: $calcDecay2;
+					border-color: orange;
+				}
+
+				&[class*="-4"] {
+					bottom: $calcDecay2;
+					right: $calcDecay1;
+					border-color: blue;
+				}
+
+			}
+
+			
+
+		}
+
 	}
 
 </style>
