93f4dba8d63eb983f97e6924d3ac004f594da32d
lio <joechipjoechip@gmail.com>
2022-01-13 12:51:48 +0100

    ethic create

---
+++ components/ethic.vue
@@ -0,0 +1,37 @@
+<template>
+	<div>
+		<micro-title :text="mainTitle" :sub-text="subTitle"/>
+	</div>
+</template>
+
+<script>
+
+	import MicroTitle from "@/components/micro/title.vue";
+
+	export default {
+
+		components: {
+			"micro-title": MicroTitle
+		},
+
+		props: {
+			
+			mainTitle: {
+				type: String,
+				required: true
+			},
+
+			subTitle: {
+				type: String,
+				default: ""
+			}
+
+		}
+
+	}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
