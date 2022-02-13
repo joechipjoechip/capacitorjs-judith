de70c45491dcde30cb7ef7b40d64bd254d5d1fb0
lio <joechipjoechip@gmail.com>
2022-02-13 18:58:01 +0100

    externalise case data to wording.js

--- pages/cases.vue
+++ pages/cases.vue
@@ -12,6 +12,8 @@
 
 	import CaseViewer from "@/components/case-viewer.vue"
 
+	import wording from "@/assets/data/wording.js";
+
 	export default {
 
 		layout: "mainLayout",
@@ -24,92 +26,8 @@
 			return {
 
 				viewerIsActive: true,
+				cases: wording.cases
 
-				cases: [
-					{
-						id: "toyota",
-						name: "Toyota Yaris",
-						year: 2021,
-						thumbnails: [
-							{
-								extension: "png",
-								assetName: "toyota-thumb-01"
-							},
-							{
-								extension: "png",
-								assetName: "toyota-thumb-02"
-							},
-							{
-								extension: "gif",
-								assetName: "toyota-thumb-03"
-							}
-						],
-						content: {
-							cta: {
-								wording: "watch clip",
-								link: "http://www.toyota.com"
-							},
-							description: [
-								"My own goal was to represent the path of life within the lushness. I achieved this through the representation of the circle of life. The fertilisation and exuberance beside the chaotic birth of a garden.",
-								"Banjo soundscapes has drawn the sound of this garden led by a post-romantic piano music piece. Kaleidoscopic arpeggios and hammering noises illustrate the blossoming and climbing flowers and plants."
-							],
-							credits: {
-								type: "CGI Animation",
-								entities: [
-									{
-										name: "Madarin Oriental Ritz Madrid",
-										whichIs: "client"
-									},
-									{
-										name: "Cannes",
-										whichIs: "creative direction"
-									},
-									{
-										name: "Sol 1 grand prix",
-										whichIs: "3d artist"
-									},
-									{
-										name: "OJO",
-										whichIs: "sound"
-									}
-								]
-							},
-							medias: {
-								cover: {
-									extension: "jpg",
-									assetName: "toyota-cover"
-								},
-								parallax: {
-									extension: "jpg",
-									assetName: "toyota-parallax"
-								},
-								lines: [
-									[
-										{
-											extension: "jpg",
-											assetName: "toyota-line-1-visu-1"
-										},
-										{
-											extension: "png",
-											assetName: "toyota-line-1-visu-2"
-										}
-									],
-									[
-										{
-											extension: "jpg",
-											assetName: "toyota-line-1-visu-1"
-										},
-										{
-											extension: "png",
-											assetName: "toyota-line-1-visu-2"
-										}
-									]
-								],
-								bottom: "toyota-bottom"
-							}
-						}
-					}
-				]
 			}
 		}	
 
