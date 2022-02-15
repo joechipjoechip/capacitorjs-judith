fc0d9407ee1b360abe8ac09d3e35f89f9619ff8e
Miel2000 <fabien.orsn@gmail.com>
2022-02-15 11:32:39 +0100

    case listing, css + bg project-container-index on mouse over

--- components/cases-listing.vue
+++ components/cases-listing.vue
@@ -1,137 +1,176 @@
 <template>
-  <div class="project-listing-container">
-      <div v-for="(project, index) in this.cases" :key="project.id">
+	<div class="project-listing-container">
+		<div v-for="(project, index) in this.cases" :key="project.id">
 
-          <div class="project-container" :class="'project-'+ project.id">
+			<div class="project-container" :class="{ 'active' : currentProject === project.id} ">
 
-              <div class="project-id">
+				<div class="project-id-container">
 
-                  <p>{{index + 1}}</p>
+					<p>{{index + 1}}</p>
 
-              </div>
+				</div>
 
-              <div class="project-name">
+				<div class="project-name-container">
 
-                  <p @mouseover="mouseTitre">{{ project.name }}</p>
+					<p 
+						@mouseenter="enterNameHandler" 
+						@mouseleave="leaveNameHandler" 
+						:data-index-project="project.id"
+					>
+						{{ project.name }}
+					</p>
 
-              </div>
-
-
-              <div class="project-year">
-
-                  <p>{{ project.year }}</p>
-
-              </div>
-
-              <div class="project-images">
-
-                  <div v-for="image in project.thumbnails" :key="image.id">
-
-                      <img class="project-image" :src="'/images/cases/' + project.id + '/' + image.assetName + '.' + image.extension" alt="">
-
-                  </div>
-
-              </div>
-
-          </div>
+				</div>
 
 
-      </div>
-      
-  </div>
+				<div class="project-year-container">
+
+					<p>{{ project.year }}</p>
+
+				</div>
+
+				<div class="project-medias-container">
+
+					<div class="project-image-container" v-for="image in project.thumbnails" :key="image.id">
+
+						<img class="project-image-content" :src="'/images/cases/' + project.id + '/' + image.assetName + '.' + image.extension" alt="">
+
+					</div>
+
+				</div>
+
+			</div>
+
+
+		</div>
+		
+	</div>
 </template>
 
 <script>
 export default {
-    layout: "mainLayout",
+	layout: "mainLayout",
 
-    props: {
 
-        cases: {
-            type: Array,
-            required: true
-        }
+	data(){
+		return {
+			currentProject: ''
 
-    },
+		}
+	},
 
-    methods: {
-        mouseTitre(event) {
-            console.log('bien enter', event.target)
-    
-        }
-    }
+
+	props: {
+
+		cases: {
+			type: Array,
+			required: true
+		}
+
+	},
+
+	methods: {
+		enterNameHandler(event) {
+			this.currentProject =  event.target.getAttribute('data-index-project');
+		},
+
+		leaveNameHandler() {
+			this.currentProject =  "";
+		}
+	}
 
 }
 </script>
 
 <style lang="scss" scoped>
 
-    .project-listing-container {
-        width: 100%;
-        height:100%;
-        left: 25px;
-        position: absolute;
-    }
+	.project-listing-container {
+		width: 100%;
+		height:100%;
+		left: 0;
+		position: absolute;
+	}
 
-    .project-container {
-        cursor:pointer;
+	.project-container {
+		transition: all 0.45s;
+		height: 10vh;
+		padding-bottom: 20px;
+		padding-left: 25px;
+		position: relative;
+		display: flex;
+		justify-content: row;
+		align-items: center;
+		flex-direction: row;
+		flex-wrap: nowrap ;
+		align-items: baseline;
+	}
 
-        transition: all 0.45s;
-      
-        border: solid 1px red;
+	.project-id-container,
+	.project-year-container {
+		font-family: "AktivGrotesk";
+		display: flex;
+		flex-direction: column;
+	  
+	}
 
-        position: relative;
-        display: flex;
-        justify-content: row;
-        align-items: center;
-    }
-    .project-id,
-    .project-year {
-        font-family: "AktivGrotesk";
-        display: flex;
-        flex-direction: column;
-        align-self: flex-end;
-        margin-bottom:18px;
-    }
+	.project-id-container {
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-end;
+		font-size: 1vw;
+		height: 100%;
+		padding-right: 10px;
+		width: 1vh;
 
-    .project-id {
-       
-        font-size: 1vw;
-        padding-right: 10px;
-        height: 100%;
+	}
 
-    }
-    .project-name {
-        align-self: flex-end;
-        margin-bottom: 1vh;
-        font-family: "AktivGrotesk";
-        font-weight: 300;
-        font-size: 5vw;
-        text-transform: uppercase; 
-        margin-right:15px;
-    }
+	.project-name-container {
+		font-family: "AktivGrotesk";
+		text-transform: uppercase; 
+		font-weight: 300;
+		font-size: 5vw;
+		margin-right:15px;
+		cursor:pointer;
+		p {
+			white-space: nowrap;
 
-    .project-year {
-        font-size: 1vw;
- 
-    }
+		}
+	}
 
-    .project-images {
-        border: solid 1px blue;
-        justify-content: flex-start;
-        width: 30%;
-        align-items: center;
-        object-fit: cover;
-        display: flex;
-        img {
-            padding-right:20px;
-        }
+	.project-year-container {
 
-    }
+		font-size: 1vw;
+
+	}
+
+	.project-medias-container {
+		height: 100%;
+		display: flex;
+		align-items: center;
+
+		.project-image-container {
+			padding-right: 2%;
+			width: 9%;
+			height: auto;         
+		}
+		
+		.project-image-content {
+			
+			object-fit: cover;
+			display: block;
+			padding-right: 20px;
+			height: 100%;
+			width: 100%;
+	
+		}
 
 
-    .project-image {
-        width: 10vw;
-    }
+	}
+
+	.active {
+		background-color: rgba(255, 251, 251, 0.795)
+	}
+
+
 
 </style>
