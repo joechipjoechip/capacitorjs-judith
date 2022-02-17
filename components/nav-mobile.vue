0057bc9a2e27844f3517c9fa5714dcacfb7aa51a
lio <joechipjoechip@gmail.com>
2022-02-17 16:06:10 +0100

    mobile menu : ok

--- components/nav-mobile.vue
+++ components/nav-mobile.vue
@@ -2,6 +2,30 @@
 
 	<div class="nav-mobile-container">
 
+		<!-- header -->
+		<transition name="transition-logo-j">
+
+			<!-- <div v-if="!displayMenuMobile" class="mobile-wrapper"> -->
+			<div class="header-wrapper">
+
+				<div class="header-logo">
+
+					<menu-logo-j  />
+
+				</div>
+
+				<div  @click="openHandler" class="header-toggle">
+
+					<menu-lines  />
+
+				</div>
+
+			</div>
+
+		</transition>
+
+
+		<!-- panel -->
 		<transition name="transition-nav-mobile">
 
 			<div v-if="displayMenuMobile" class="panel-wrapper">
@@ -31,26 +55,6 @@
 						</ul>
 
 					</nav>
-
-				</div>
-
-			</div>
-
-		</transition>
-
-		<transition name="transition-logo-j">
-
-			<div v-if="!displayMenuMobile" class="mobile-wrapper">
-
-				<div class="logo-j-mobile">
-
-					<menu-logo-j  />
-
-				</div>
-
-				<div  @click="openHandler" class="toggle-menu-mobile">
-
-					<menu-lines  />
 
 				</div>
 
@@ -107,107 +111,107 @@ export default {
 
 	.nav-mobile-container {
 
-		.mobile-wrapper {
-				z-index:501;
+		.header-wrapper {
+			z-index:501;
+			display:flex;
+			flex-direction: row;
+			height: 90px;
+	
+			.header-logo {
+				
 				display:flex;
-				flex-direction: row;
-				height: 90px;
+				padding: 20px 0;
+				margin: 0 auto;
+			}
 		
-				.logo-j-mobile {
-					
-					display:flex;
-					padding: 20px 0;
+			.header-toggle {
+				color: var(--color-primary);
+				position: absolute;
+				height: auto;
+				height: 90px;
+				right: 4px;
+				top: -5px;
+				display: flex;
+				align-items: center;
+			}
+		}
+
+		.panel-wrapper {
+
+			z-index: 510;
+			position: fixed;
+			right:0;
+			top:0;
+			width: 100%;
+			height: 100vh;
+
+			display: flex;
+			flex-direction: row;
+			justify-content: flex-end;
+
+
+			// background-color:rgba(0, 0, 0, 0.75);
+
+			.panel-logo-container {
+				z-index: 600;
+
+				position: absolute;
+				height: 90px;
+				width: 80%;
+
+				svg {
+					width: 40%;
+					display: block;
 					margin: 0 auto;
 				}
-			
-				.toggle-menu-mobile{
-					color: var(--color-primary);
-					position: absolute;
-					height: auto;
-					height: 90px;
-					right: 4px;
-					top: -5px;
-					display: flex;
-					align-items: center;
-				}
+
 			}
 
-			.panel-wrapper {
-
-			
-				z-index: 510;
-				position: fixed;
-				right:0;
-				width: 100%;
-				height: 100vh;
-
+			.panel-nav-container {
+				justify-content: space-around;
+				flex-flow: column;
 				display: flex;
-				flex-direction: row;
-				justify-content: flex-end;
+				height: 100%;
+				width: 80%;
 
-
-				// background-color:rgba(0, 0, 0, 0.75);
-
-				.panel-logo-container {
-					z-index: 600;
-
-					position: absolute;
-					height: 90px;
-					width: 80%;
-
-					svg {
-						width: 40%;
-						display: block;
-						margin: 0 auto;
-					}
-
-				}
-
-				.panel-nav-container {
-					justify-content: space-around;
-					flex-flow: column;
+				nav {
+					border-left: solid 1px rgba(255,255,255, 0.4);
 					display: flex;
+					justify-content: center;
+					flex-direction: column;
+					width: 100%;
 					height: 100%;
-					width: 80%;
+					background-color: #000;
 
-					nav {
-						border-left: solid 1px rgba(255,255,255, 0.4);
-						display: flex;
-						justify-content: center;
-						flex-direction: column;
-						width: 100%;
-						height: 100%;
-						background-color: #000;
+					a {
+						color:white;
+						font-family: 'AktivGrotesk';
+						text-decoration: none;
+						text-transform: uppercase;
 
-						a {
-							color:white;
-							font-family: 'AktivGrotesk';
-							text-decoration: none;
-							text-transform: uppercase;
-
-						}
-
-						ul {
-							margin-left: 40px;
-							margin-left: 40px;
-							height: 20%;
-							display: flex;
-							flex-flow: column nowrap;
-							justify-content: space-between;
-
-							li {
-								// 
-							}
-
-						}
-
-					
 					}
 
+					ul {
+						margin-left: 40px;
+						margin-left: 40px;
+						height: 20%;
+						display: flex;
+						flex-flow: column nowrap;
+						justify-content: space-between;
+
+						li {
+							// 
+						}
+
+					}
+
+				
 				}
-	
 
 			}
+
+
+		}
 	}
 
 </style>
